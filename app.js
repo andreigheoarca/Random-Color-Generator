6f20ef69a886654c90afa254b78966aa51dca3d8
andreigheoarca <andreigheoarca@gmail.com>
2023-02-05 23:11:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log("Random color generator");
+
+const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+console.log(hexArray);
+
+const generateBtn = document.querySelector(".btn");
+const colorBox = document.querySelector(".color-box");
+// console.log(generateBtn, colorBox);
+
+function randomHexGenerator() {
+  let hexValue = "#";
+  for (let i = 0; i < 6; i++) {
+    // console.log(i);
+    hexValue += hexArray[randomNumberGenerator()];
+    console.log(hexValue);
+  }
+  return hexValue;
+}
+randomHexGenerator();
+
+function randomNumberGenerator() {
+  let randomNumber = Math.ceil(Math.random() * hexArray.length);
+  //   console.log(randomNumber);
+  return randomNumber;
+}
+
+generateBtn.addEventListener("click", function () {
+  //   console.log(colorBox);
+  colorBox.style.backgroundColor = randomHexGenerator();
+});
